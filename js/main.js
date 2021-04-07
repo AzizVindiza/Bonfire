bac5d047bf284e1823efc1aca2c5de1b0147fcb7
Denchik_1_1 <denistronin30@gmail.com>
2021-04-07 19:29:07 +0300

    first upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+swiper1 = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    watchOverflow: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+});
